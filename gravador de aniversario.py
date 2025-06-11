fc521a7a0e557e7b9dd01816206c7ca3a026a141
matheusnmattos <matheusnmattos@gmail.com>
2025-06-11 14:22:37 -0300

    Add files via upload

---
+++ gravador de aniversario.py
@@ -0,0 +1,4 @@
+dia=input('digite o dia que você nasceu')
+mês=input('escreva o mês em que você nasceu')
+ano=input('digite o ano em que você nasceu')
+print('você nasceu no dia' ,dia, ' de ' ,mês,'do ano' ,ano,'.Correto?')
---
+++ somador.py
@@ -0,0 +1,3 @@
+numero1=input('digite o primeiro numero pra ser somado')
+numero2=input('digite o segundo numero pra ser somado')
+print(numero1+numero2)
